5edf1200b03008b6cf6eed394e09be9d818cd2af
fff02 <fff02@yandex.ru>
2023-09-25 03:41:25 +0300

    [RU] Асинхронный код с пробежкой по файлам и суммированием чисел в каждом из них [ENG] My async code is written to run through the files and to sum the digits contained in them.

--- Async_python_10_9/main.py
+++ Async_python_10_9/main.py
@@ -4,23 +4,27 @@ from aiofiles import os as aos
 import time
 
 f_summa = 0
-async def read_f(fname: str,sem:asyncio.Semaphore) -> None:
+
+
+async def read_f(fname: str, sem: asyncio.Semaphore) -> None:
     global f_summa
     async with sem:
-        async with aiofiles.open(file=fname,mode='r',encoding='utf-8') as f:
+        async with aiofiles.open(file=fname, mode='r', encoding='utf-8') as f:
             content = int((await f.readline()).rstrip())
-            if content%2==0:
-                f_summa +=content
+            if content % 2 == 0:
+                f_summa += content
 
 
 async def main():
     path: str = r"C:\Users\***\PycharmProjects\forstepik\files"
     sem: asyncio.Semaphore = asyncio.Semaphore(1000)
     temp_check = await aos.listdir(path)
-    tasks = [asyncio.create_task(read_f(f"{path}\{filename}",sem)) for filename in temp_check]
+    tasks = [asyncio.create_task(read_f(f"{path}\{filename}", sem)) for filename in temp_check]
     start_time = time.time()
     await asyncio.gather(*tasks)
     end_time = time.time()
-    print(f"Time: {end_time-start_time}")
+    print(f"Time: {end_time - start_time}")
     print(f_summa)
+
+
 asyncio.run(main())
